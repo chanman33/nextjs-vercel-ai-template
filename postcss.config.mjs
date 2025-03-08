257c254c07cf5620239398af19a114d8da535f3d
chanman33 <chandler.j.ward@gmail.com>
2025-03-08 16:22:27 -0700

    wip: resolve global.css errors - pending

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
-const config = {
+/** @type {import('postcss-load-config').Config} */
+export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
-
-export default config;
